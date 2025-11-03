31b850d67eb086104cc1b66b34aa1ee0faf6244a
Okezie <okezievictor45@gmail.com>
2025-11-03 06:10:25 +0100

    Initial Commit

---
+++ backend/api/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Note(models.Model):
+    title = models.CharField(max_length=70)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
