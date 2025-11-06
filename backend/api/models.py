1e4611102d913c20a87df5df5ad261b85e3ea0eb
Okezie <okezievictor45@gmail.com>
2025-11-06 08:16:08 +0100

    updated urls.py/api

--- backend/api/models.py
+++ backend/api/models.py
@@ -6,3 +6,4 @@ class Note(models.Model):
     title = models.CharField(max_length=70)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notes")
